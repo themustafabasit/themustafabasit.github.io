e9b3e802ecf048417c35d1c975e26e8894394f61
Mustafa Basit <mailtobasit74@gmail.com>
2020-04-24 15:27:59 +0530

    Handled submit action by JS, Added footer margin

--- js/main.js
+++ js/main.js
@@ -13,3 +13,15 @@ function doTask(){
   //specify below the delay of download in micro seconds
   setTimeout(function(){window.open('./resources/resume.pdf')}, 2000);
 }
+
+function form_action(){
+  alert("Thanks for messaging Me,\n" + "Currently I'm Recieving Forms via E-mail. An App will open in a while, do HIT that \"Send\" button");
+  
+  var name = document.getElementById('sender_name').value;  
+  var mailid = document.getElementById('sender_mailid').value;
+  var message = "Username: " + name + "<br>"+ "<br>Message: <br>"+ document.getElementById('sender_message').value;
+  
+  var mailto = "mailto:mailtobasit74@gmail.com?subject=" + "New mail from Portfolio by -\"" + name + "\"&body=" + message + "<br><br>Reply Me here: " + mailid;
+  window.open(mailto);
+
+}
